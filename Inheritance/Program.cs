4206f632f405c01861fbb0b07a6be414dfcdafef
selormbennette <bsbobie@st.ug.edu.gh>
2024-07-04 12:59:03 +0100

    Completed Inheritance and Method Overriding

---
+++ Inheritance/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+class Animal
+{
+    public virtual void MakeSound()
+    {
+        Console.WriteLine("Some generic sound");
+    }
+}
+
+class Dog : Animal
+{
+    public override void MakeSound()
+    {
+        Console.WriteLine("Bark");
+    }
+}
+
+class Cat : Animal
+{
+    public override void MakeSound()
+    {
+        Console.WriteLine("Meow");
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Animal animal = new Animal();
+        animal.MakeSound(); 
+
+        Dog dog = new Dog();
+        dog.MakeSound(); 
+
+        Cat cat = new Cat();
+        cat.MakeSound();
+    }
+}
+
