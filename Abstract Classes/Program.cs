fb59107a5a3fec06df8c044dd312043b7d57ef3f
selormbennette <bsbobie@st.ug.edu.gh>
2024-07-04 13:03:06 +0100

    Completed Abstract Classes and Methods

---
+++ Abstract Classes/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+abstract class Shape
+{
+    public abstract double GetArea();
+}
+
+class Circle : Shape
+{
+    private double radius;
+
+    public Circle(double radius)
+    {
+        this.radius = radius;
+    }
+
+    public override double GetArea()
+    {
+        return Math.PI * radius * radius;
+    }
+}
+
+class Rectangle : Shape
+{
+    private double width;
+    private double height;
+
+    public Rectangle(double width, double height)
+    {
+        this.width = width;
+        this.height = height;
+    }
+
+    public override double GetArea()
+    {
+        return width * height;
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Circle circle = new Circle(5);
+        Console.WriteLine($"Area of Circle: {circle.GetArea()}");
+
+        Rectangle rectangle = new Rectangle(6, 8);
+        Console.WriteLine($"Area of Rectangle: {rectangle.GetArea()}");
+    }
+}
+
