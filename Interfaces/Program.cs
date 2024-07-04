bdaaa851a4c98829a962512eac89cdda04852197
selormbennette <bsbobie@st.ug.edu.gh>
2024-07-04 13:04:20 +0100

    Completed Interfaces

---
+++ Interfaces/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Interfaces
+{
+    
+    interface IMovable
+    {
+        void Move();
+    }
+
+   
+    class Car : IMovable
+    {
+        public void Move()
+        {
+            Console.WriteLine("Car is moving");
+        }
+    }
+
+   
+    class Bicycle : IMovable
+    {
+        public void Move()
+        {
+            Console.WriteLine("Bicycle is moving");
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            IMovable car = new Car();
+            IMovable bicycle = new Bicycle();
+
+            car.Move();
+            bicycle.Move();
+        }
+    }
+}
+
